5d1a0ab781a5dd60073672c3f52dd20c9fee4b4c
NickChecan <nicholas.checan@icloud.com>
2024-11-03 23:08:57 -0600

    test: implement initial test for createPartControl

--- com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/ui/ChatViewTest.java
+++ com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/ui/ChatViewTest.java
@@ -1,30 +1,46 @@
 package com.developer.nefarious.eclipse.copilot.test.ui;
 
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.developer.nefarious.eclipse.copilot.ui.NewChatView;
 
 public class ChatViewTest {
-	
+
 	private NewChatView cut;
-	
+
+	@Mock
+	private Browser mockBrowser;
+
+	@Mock
+	private Composite mockParent;
+
 	@BeforeEach
 	public void setUp() {
-		cut = new NewChatView();
+		MockitoAnnotations.openMocks(this);
+
+		cut = spy(new NewChatView());
+		cut.setBrowser(mockBrowser);
 	}
-	
+
 	@Test
 	public void testCreatePartControl() {
 		// Arrange
-		Composite mockParent = Mockito.mock(Composite.class);
+
 		// Act
 		cut.createPartControl(mockParent);
+
 		// Assert
 		Assertions.assertTrue(true);
+		verify(mockBrowser).setText("test");
 	}
 
 }
--- com.developer.nefarious.eclipse.copilot/src/com/developer/nefarious/eclipse/copilot/ui/NewChatView.java
+++ com.developer.nefarious.eclipse.copilot/src/com/developer/nefarious/eclipse/copilot/ui/NewChatView.java
@@ -1,12 +1,20 @@
 package com.developer.nefarious.eclipse.copilot.ui;
 
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 
 public class NewChatView extends ViewPart implements ISelectionListener {
+	
+	private Browser browser;
+	
+	public void setBrowser(Browser browser) {
+		this.browser = browser;
+	}
 
 	@Override
 	public void selectionChanged(IWorkbenchPart arg0, ISelection arg1) {
@@ -15,8 +23,9 @@ public class NewChatView extends ViewPart implements ISelectionListener {
 	}
 
 	@Override
-	public void createPartControl(Composite arg0) {
-		// TODO Auto-generated method stub
+	public void createPartControl(Composite parent) {
+		browser = browser != null ? browser : new Browser(parent, SWT.WEBKIT);
+		browser.setText("test");
 		
 	}
 
