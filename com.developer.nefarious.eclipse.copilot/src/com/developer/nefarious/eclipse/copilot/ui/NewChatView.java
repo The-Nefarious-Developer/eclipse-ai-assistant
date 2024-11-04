4cbb000002b8bb3b8119e4e845e14fc5d261d974
NickChecan <nicholas.checan@icloud.com>
2024-11-04 00:18:31 -0600

    chore: create the css file

--- com.developer.nefarious.eclipse.copilot/src/com/developer/nefarious/eclipse/copilot/ui/NewChatView.java
+++ com.developer.nefarious.eclipse.copilot/src/com/developer/nefarious/eclipse/copilot/ui/NewChatView.java
@@ -1,5 +1,9 @@
 package com.developer.nefarious.eclipse.copilot.ui;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
@@ -9,9 +13,9 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 
 public class NewChatView extends ViewPart implements ISelectionListener {
-	
+
 	private Browser browser;
-	
+
 	public void setBrowser(Browser browser) {
 		this.browser = browser;
 	}
@@ -19,20 +23,20 @@ public class NewChatView extends ViewPart implements ISelectionListener {
 	@Override
 	public void selectionChanged(IWorkbenchPart arg0, ISelection arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
 		browser = browser != null ? browser : new Browser(parent, SWT.WEBKIT);
 		browser.setText("test");
-		
+
 	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
