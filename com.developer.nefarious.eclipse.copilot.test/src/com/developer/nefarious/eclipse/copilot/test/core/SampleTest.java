37e6fa16501311edbbc785f853b9ad6e37ed7df4
NickChecan <nicholas.checan@icloud.com>
2024-11-03 21:59:38 -0600

    refactor: rename test core package

--- com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/core/SampleTest.java
+++ com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/core/SampleTest.java
@@ -1,4 +1,4 @@
-package com.developer.nefarious.eclipse.copilot.core;
+package com.developer.nefarious.eclipse.copilot.test.core;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
