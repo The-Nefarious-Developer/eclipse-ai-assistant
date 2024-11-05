c5dc0913f421d962509ef66a77ee1d44a3d5fe06
NickChecan <nicholas.checan@icloud.com>
2024-11-04 20:54:49 -0600

    refactor: prepare project for powermock installation

--- com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/ui/ChatViewTest.java
+++ com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/ui/ChatViewTest.java
@@ -1,45 +1,42 @@
 package com.developer.nefarious.eclipse.copilot.test.ui;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.developer.nefarious.eclipse.copilot.ui.NewChatView;
+import com.developer.nefarious.eclipse.copilot.ui.ChatView;
 
 public class ChatViewTest {
 
-	private NewChatView cut;
+	private ChatView cut;
 
 	@Mock
 	private Browser mockBrowser;
-
-	@Mock
-	private Composite mockParent;
 
 	@BeforeEach
 	public void setUp() {
 		MockitoAnnotations.openMocks(this);
 
-		cut = spy(new NewChatView());
+		cut = spy(new ChatView());
 		cut.setBrowser(mockBrowser);
 	}
 
 	@Test
 	public void testCreatePartControl() {
 		// Arrange
+		Composite mockParent = mock(Composite.class);
 
 		// Act
 		cut.createPartControl(mockParent);
 
 		// Assert
-		Assertions.assertTrue(true);
 		verify(mockBrowser).setText("test");
 	}
 
--- com.developer.nefarious.eclipse.copilot/src/com/developer/nefarious/eclipse/copilot/ui/IChatViewHelper.java
+++
@@ -1,7 +0,0 @@
-package com.developer.nefarious.eclipse.copilot.ui;
-
-public interface IChatViewHelper {
-	
-	String getContent();
-
-}
