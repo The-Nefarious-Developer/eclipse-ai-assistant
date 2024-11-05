0eaf47e0ec65b9d5a9f90562e3babbb57ebdac5c
NickChecan <nicholas.checan@icloud.com>
2024-11-04 23:43:14 -0600

    test: implement and tdding view render class

---
+++ com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/ui/ChatViewRenderTest.java
@@ -0,0 +1,50 @@
+package com.developer.nefarious.eclipse.copilot.test.ui;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.developer.nefarious.eclipse.copilot.ui.ChatViewRender;
+import com.developer.nefarious.eclipse.copilot.ui.ViewRender;
+
+public class ChatViewRenderTest {
+	
+	private ViewRender cut;
+
+	private String randomWord() {
+		final String[] WORDS = { "apple", "banana", "grape" };
+		int randomIndex = ThreadLocalRandom.current().nextInt(WORDS.length);
+		return WORDS[randomIndex];
+	}
+	
+	@BeforeEach
+	public void setUp() {
+		cut = spy(new ChatViewRender());
+	}
+	
+	@Test
+	public void testGetContent() {
+		// Arrange
+		String mockJsFileContent = this.randomWord();
+		doReturn(mockJsFileContent).when(cut).getResourceContent("ChatView.js");
+		String expectedJsReference = "<script>" + mockJsFileContent + "</script>";
+		
+		String mockCssFileContent = this.randomWord();
+		doReturn(mockCssFileContent).when(cut).getResourceContent("ChatView.css");
+		String expectedCssReference = "<style>" + mockCssFileContent + "</style>";
+		
+		// Act
+		String returnValue = cut.build();
+		
+		// Assert
+		assertTrue(returnValue.contains(expectedJsReference));
+		assertTrue(returnValue.contains(expectedCssReference));
+	}
+	
+	
+}
---
+++ com.developer.nefarious.eclipse.copilot/src/com/developer/nefarious/eclipse/copilot/ui/ViewRender.java
@@ -0,0 +1,9 @@
+package com.developer.nefarious.eclipse.copilot.ui;
+
+public interface ViewRender {
+
+	String build();
+	
+	String getResourceContent(String filename);
+	
+}
