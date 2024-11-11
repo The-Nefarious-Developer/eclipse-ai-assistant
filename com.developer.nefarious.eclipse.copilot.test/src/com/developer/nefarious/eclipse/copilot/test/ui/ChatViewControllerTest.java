7947c3e0ed4b3b194b1dd07b81c92df12db881e1
NickChecan <nicholas.checan@gmail.com>
2024-11-10 23:01:53 -0600

    test: first test for chat controller

--- com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/ui/ChatViewControllerTest.java
+++ com.developer.nefarious.eclipse.copilot.test/src/com/developer/nefarious/eclipse/copilot/test/ui/ChatViewControllerTest.java
@@ -1,15 +1,31 @@
 package com.developer.nefarious.eclipse.copilot.test.ui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.developer.nefarious.eclipse.copilot.ui.ChatViewController;
+import com.developer.nefarious.eclipse.copilot.ui.IChatViewController;
 
 public class ChatViewControllerTest {
 
+	private IChatViewController cut;
+
 	@BeforeEach
 	public void setUp() {
-		
+		cut = new ChatViewController();
 	}
-	
-//	@Test
-//	pub
+
+	@Test
+	public void testGetAnswerFromAI() {
+		// Arrange
+		String mockUserPrompt = "A very good prompt";
+		String expectedValue = "Hello **bosta**";
+		// Act
+		String returnValue = cut.getAnswerFromAI(mockUserPrompt);
+		// Assert
+		assertEquals(returnValue, expectedValue);
+	}
 
 }
