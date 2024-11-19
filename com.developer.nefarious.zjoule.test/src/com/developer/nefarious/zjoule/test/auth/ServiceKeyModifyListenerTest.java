45699b364db0003c351a68d24706590614cd5703
NickChecan <nicholas.checan@icloud.com>
2024-11-19 16:53:38 -0600

    test: service key modify listener

--- com.developer.nefarious.zjoule.test/src/com/developer/nefarious/zjoule/test/auth/ServiceKeyModifyListenerTest.java
+++ com.developer.nefarious.zjoule.test/src/com/developer/nefarious/zjoule/test/auth/ServiceKeyModifyListenerTest.java
@@ -1,28 +1,63 @@
 package com.developer.nefarious.zjoule.test.auth;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.eclipse.swt.events.ModifyEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import com.developer.nefarious.zjoule.auth.FirstLoginWizardPage;
 import com.developer.nefarious.zjoule.auth.ServiceKeyModifyListener;
 
 public class ServiceKeyModifyListenerTest {
 
 	private ServiceKeyModifyListener cut;
 	
+	@Mock
+	FirstLoginWizardPage mockFirstLoginWizardPage;
+	
 	@BeforeEach
 	public void setUp() {
-//		cut = new DeploymentSelectionAdapter();
+		MockitoAnnotations.openMocks(this);
+		
+		cut = new ServiceKeyModifyListener(mockFirstLoginWizardPage);
 	}
 	
 	@Test
-	public void testModifyText() {
+	public void testModifyTextWhenThereIsText() {
 		// Arrange
+		ModifyEvent mockModifyEvent = mock(ModifyEvent.class);
+		
+		String mockInputText = "Text I don't care about";
+		when(mockFirstLoginWizardPage.getInputText()).thenReturn(mockInputText);
+		
 		// Act
-//		cut.modifyText(null);
+		cut.modifyText(mockModifyEvent);
+		
 		// Assert
 		assertTrue(true);
+		verify(mockFirstLoginWizardPage).setPageComplete(true);
+	}
+	
+	@Test
+	public void testModifyTextWhenThereIsNoText() {
+		// Arrange
+		ModifyEvent mockModifyEvent = mock(ModifyEvent.class);
+		
+		String mockInputText = "";
+		when(mockFirstLoginWizardPage.getInputText()).thenReturn(mockInputText);
+		
+		// Act
+		cut.modifyText(mockModifyEvent);
+		
+		// Assert
+		assertTrue(true);
+		verify(mockFirstLoginWizardPage).setPageComplete(false);
 	}
 	
 }
