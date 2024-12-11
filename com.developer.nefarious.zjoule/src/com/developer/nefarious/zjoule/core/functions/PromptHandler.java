90181a679fb71c6fd041de4863215b629c4ed450
NickChecan <nicholas.checan@icloud.com>
2024-12-10 20:58:33 -0600

    chore: connect prompt handler to chat orchestrator

--- com.developer.nefarious.zjoule/src/com/developer/nefarious/zjoule/core/functions/PromptHandler.java
+++ com.developer.nefarious.zjoule/src/com/developer/nefarious/zjoule/core/functions/PromptHandler.java
@@ -2,11 +2,15 @@ package com.developer.nefarious.zjoule.core.functions;
 
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.BrowserFunction;
+import com.developer.nefarious.zjoule.chat.ChatOrchestrator;
 
 public class PromptHandler extends BrowserFunction {
+	
+	private ChatOrchestrator chatOrchestrator;
 
 	public PromptHandler(final Browser browser, final String name) {
 		super(browser, name);
+		chatOrchestrator = new ChatOrchestrator();
 	}
 	
 	public static PromptHandler create(final Browser browser, final String name) {
@@ -15,38 +19,9 @@ public class PromptHandler extends BrowserFunction {
 
 	@Override
 	public Object function(final Object[] arguments) {
-//		MemoryAccessToken memoryAccessToken = MemoryAccessToken.getInstance();
-//		MemoryServiceKey memoryServiceKey = MemoryServiceKey.getInstance();
-//		AuthClientHelper authHelper = new AuthClientHelper();
-//		AuthClient authClient = new AuthClient(memoryAccessToken, memoryServiceKey, authHelper);
-//		
-//		MemoryResourceGroup memoryResourceGroup = MemoryResourceGroup.getInstance();
-//		MemoryDeployment memoryDeployment = MemoryDeployment.getInstance();
-//		OpenAIClientHelper aiClientHelper = new OpenAIClientHelper();
-//		MemoryMessageHistory memoryMessageHistory = MemoryMessageHistory.getInstance();
-//		
-//		OpenAIClient aiClient = new OpenAIClient(authClient, memoryMessageHistory, memoryResourceGroup, memoryDeployment, aiClientHelper);
-//		
-//		String userPrompt = arguments[0].toString();
-//		OpenAIChatMessage message = new OpenAIChatMessage(Role.USER, userPrompt);
-//		
-//		List<OpenAIChatMessage> messages = new ArrayList<>();
-//		messages.add(message);
-//		
-//		IChatMessage answer;
-//		try {
-//			answer = aiClient.chatCompletion(messages);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return "Error during request execution";
-//		}
-//		
-		return getName() + " executed!";
-//		
-////		return arguments[0].toString();
-//		
-//		return answer.getMessage();
-//		return "";
+		String userPrompt = arguments[0].toString();
+		String answer = chatOrchestrator.getAnswer(userPrompt);
+		return answer;
 	}
 
 }
