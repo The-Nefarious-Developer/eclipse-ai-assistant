fb4b8bcea594372d386230cd315721cc930db5ed
NickChecan <nicholas.checan@icloud.com>
2024-12-10 20:53:45 -0600

    test: implement regression tests for chat orchestrator

--- com.developer.nefarious.zjoule.test/src/com/developer/nefarious/zjoule/test/chat/ChatOrchestratorTest.java
+++ com.developer.nefarious.zjoule.test/src/com/developer/nefarious/zjoule/test/chat/ChatOrchestratorTest.java
@@ -1,11 +1,14 @@
 package com.developer.nefarious.zjoule.test.chat;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
@@ -22,56 +25,108 @@ import com.developer.nefarious.zjoule.chat.IChatOrchestrator;
 import com.developer.nefarious.zjoule.models.Role;
 
 public class ChatOrchestratorTest {
-	
+
 	private IChatOrchestrator cut;
-	
+
 	MockedStatic<AIClientFactory> mockAIClientFactory;
-	
+
 	@Mock
 	private IAIClient mockAIClient;
-	
+
 	@BeforeEach
 	public void setUp() {
 		MockitoAnnotations.openMocks(this);
-		
+
 		mockAIClientFactory = mockStatic(AIClientFactory.class);
 		mockAIClientFactory.when(AIClientFactory::getClient).thenReturn(mockAIClient);
-		
+
 		cut = new ChatOrchestrator();
 	}
-	
+
 	@AfterEach
 	public void tearDown() {
 		if (mockAIClientFactory != null) {
 			mockAIClientFactory.close();
 		}
 	}
-	
+
 	@Test
 	public void shouldPlumbAnswer() throws IOException, InterruptedException {
 		// Arrange
 		String mockUserPrompt = "What's the meaning of life?";
 		IChatMessage mockUserMessage = mock(IChatMessage.class);
 		when(mockAIClient.createMessage(Role.USER, mockUserPrompt)).thenReturn(mockUserMessage);
-		
+
 		IChatMessage mockOldMessage1 = mock(IChatMessage.class);
 		IChatMessage mockOldMessage2 = mock(IChatMessage.class);
 		List<IChatMessage> mockMessageHistory = Arrays.asList(mockOldMessage1, mockOldMessage2);
 		when(mockAIClient.getMessageHistory()).thenReturn(mockMessageHistory);
-		
+
 		List<IChatMessage> mockInputMessages = Arrays.asList(mockOldMessage1, mockOldMessage2, mockUserMessage);
-		
 		IChatMessage mockAnswer = mock(IChatMessage.class);
 		when(mockAIClient.chatCompletion(mockInputMessages)).thenReturn(mockAnswer);
-		
-		List<IChatMessage> mockAllMessages = Arrays.asList(mockOldMessage1, mockOldMessage2, mockUserMessage, mockAnswer);
-		
+
+		List<IChatMessage> mockAllMessages = Arrays.asList(mockOldMessage1, mockOldMessage2, mockUserMessage,
+				mockAnswer);
+
 		String expectedValue = "42";
 		when(mockAnswer.getMessage()).thenReturn(expectedValue);
-		
+
 		// Act
 		String returnValue = cut.getAnswer(mockUserPrompt);
-		
+
+		// Assert
+		verify(mockAIClient).setMessageHistory(mockAllMessages);
+		assertEquals(expectedValue, returnValue);
+	}
+
+	@Test
+	public void shouldPlumbErrorMessageIfChatCompletionDoesntWork() throws IOException, InterruptedException {
+		// Arrange
+		String mockUserPrompt = "What's the meaning of life?";
+		IChatMessage mockUserMessage = mock(IChatMessage.class);
+		when(mockAIClient.createMessage(Role.USER, mockUserPrompt)).thenReturn(mockUserMessage);
+
+		IChatMessage mockOldMessage = mock(IChatMessage.class);
+		List<IChatMessage> mockMessageHistory = Arrays.asList(mockOldMessage);
+		when(mockAIClient.getMessageHistory()).thenReturn(mockMessageHistory);
+
+		List<IChatMessage> mockInputMessages = Arrays.asList(mockOldMessage, mockUserMessage);
+		when(mockAIClient.chatCompletion(mockInputMessages)).thenThrow(new IOException("An error occurred"));
+
+		String expectedValue = "Error during the AI request execution";
+
+		// Act
+		String returnValue = cut.getAnswer(mockUserPrompt);
+
+		// Assert
+		verify(mockAIClient, never()).setMessageHistory(any());
+		assertEquals(expectedValue, returnValue);
+	}
+
+	@Test
+	public void shouldNotStopIfThereIsNoMessageHistory() throws IOException, InterruptedException {
+		// Arrange
+		String mockUserPrompt = "What's the meaning of life?";
+		IChatMessage mockUserMessage = mock(IChatMessage.class);
+		when(mockAIClient.createMessage(Role.USER, mockUserPrompt)).thenReturn(mockUserMessage);
+
+		List<IChatMessage> mockMessageHistory = new ArrayList<IChatMessage>();
+		when(mockAIClient.getMessageHistory()).thenReturn(mockMessageHistory);
+
+		List<IChatMessage> mockInputMessages = Arrays.asList(mockUserMessage);
+
+		IChatMessage mockAnswer = mock(IChatMessage.class);
+		when(mockAIClient.chatCompletion(mockInputMessages)).thenReturn(mockAnswer);
+
+		List<IChatMessage> mockAllMessages = Arrays.asList(mockUserMessage,	mockAnswer);
+
+		String expectedValue = "42";
+		when(mockAnswer.getMessage()).thenReturn(expectedValue);
+
+		// Act
+		String returnValue = cut.getAnswer(mockUserPrompt);
+
 		// Assert
 		verify(mockAIClient).setMessageHistory(mockAllMessages);
 		assertEquals(expectedValue, returnValue);
