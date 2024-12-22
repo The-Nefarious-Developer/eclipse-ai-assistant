abb0408a8a4261b0d317930c03f81b29f2a98afc
NickChecan <nicholas.checan@icloud.com>
2024-12-21 18:13:41 -0600

    fix: update all necessary files for new release

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
+# Check if the version argument is provided
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <new_version>"
+  exit 1
+fi
+
+# Assign the new version from the argument
+NEW_VERSION="$1"
+
 # Navigate to the parent folder of the script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 cd "$PARENT_DIR" || { echo "Error: Failed to navigate to parent folder."; exit 1; }
 
-# Update the root directory pom and its reference on the test import
-mvn versions:set -DnewVersion=2.0.0 -DprocessAllModules -DgenerateBackupPoms=false
+# # Update the root directory pom and its reference on the test import
+./mvnw org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=${NEW_VERSION} -DprocessAllModules
 
 # Update the pom and the manifest of the plugin module
-mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=2.0.0 -pl com.developer.nefarious.zjoule.plugin
+./mvnw org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=${NEW_VERSION} -pl com.developer.nefarious.zjoule.plugin -am
 
 # Update the pom and the manifest of the test module
-mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=2.0.0 -pl com.developer.nefarious.zjoule.test
+./mvnw org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=${NEW_VERSION} -pl com.developer.nefarious.zjoule.test -am
+./mvnw versions:use-dep-version -Dincludes=com.developer.nefarious.zjoule:com.developer.nefarious.zjoule.plugin -DdepVersion=${NEW_VERSION} -DforceVersion=true -DgenerateBackupPoms=false -pl com.developer.nefarious.zjoule.test
 
 # Update the pom and the feature file of the feature module
-mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=2.0.0 -pl com.developer.nefarious.zjoule.feature
+./mvnw org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=${NEW_VERSION} -pl com.developer.nefarious.zjoule.feature -am
 
 # Update the pom of the updatesite module
-mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=2.0.0 -pl com.developer.nefarious.zjoule.updatesite
+./mvnw org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=${NEW_VERSION} -pl com.developer.nefarious.zjoule.updatesite -am
 
 # Update the category.xml file with the new version at the updatesite module
 chmod +x ./scripts/update-category.sh
-./scripts/update-category.sh 2.0.0
+./scripts/update-category.sh ${NEW_VERSION}
