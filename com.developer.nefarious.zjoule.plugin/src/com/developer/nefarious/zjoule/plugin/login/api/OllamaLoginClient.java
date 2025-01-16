8fa4a4f61137cfc2aa6191708df181d981091bdf
nickchecan <nickchecan@localhost>
2025-01-15 22:15:38 -0600

    chore: create ollama endpoint memory object

--- com.developer.nefarious.zjoule.plugin/src/com/developer/nefarious/zjoule/plugin/login/api/OllamaLoginClient.java
+++ com.developer.nefarious.zjoule.plugin/src/com/developer/nefarious/zjoule/plugin/login/api/OllamaLoginClient.java
@@ -1,10 +1,20 @@
 package com.developer.nefarious.zjoule.plugin.login.api;
 
+import java.net.http.HttpClient;
+
 public class OllamaLoginClient implements IOllamaLoginClient {
+	
+	private HttpClient httpClient;
+	
+	private IOllamaLoginClientHelper ollamaLoginClientHelper;
+	
+	public OllamaLoginClient(final IOllamaLoginClientHelper ollamaLoginClientHelper) {
+		httpClient = HttpClient.newHttpClient();
+		this.ollamaLoginClientHelper = ollamaLoginClientHelper;
+	}
 
 	@Override
 	public GetOllamaModelsResponse getModels() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
---
+++ com.developer.nefarious.zjoule.plugin/src/com/developer/nefarious/zjoule/plugin/memory/MemoryOllamaEndpoint.java
@@ -0,0 +1,51 @@
+package com.developer.nefarious.zjoule.plugin.memory;
+
+public class MemoryOllamaEndpoint implements IMemoryObject<String> {
+	
+    public static final String KEY = "ollama-endpoint";
+
+    private static MemoryOllamaEndpoint instance;
+
+    private IEclipseMemory eclipseMemory;
+
+    public static MemoryOllamaEndpoint getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("MemoryOllamaEndpoint not initialized. Call initialize() first.");
+        }
+        return instance;
+    }
+
+    public static void initialize(final IEclipseMemory eclipseMemory) {
+        if (instance == null) {
+            instance = new MemoryOllamaEndpoint(eclipseMemory);
+        }
+    }
+
+    public static void resetInstance() {
+        instance = null;
+    }
+
+    private MemoryOllamaEndpoint(final IEclipseMemory eclipseMemory) {
+        this.eclipseMemory = eclipseMemory;
+    }
+
+    @Override
+    public Boolean isEmpty() {
+        String ollamaEndpoint = load();
+        if ((ollamaEndpoint == null) || ollamaEndpoint.isEmpty() || ollamaEndpoint.isBlank()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String load() {
+        return eclipseMemory.loadFromEclipsePreferences(KEY);
+    }
+
+    @Override
+    public void save(final String ollamaEndpoint) {
+        eclipseMemory.saveOnEclipsePreferences(KEY, ollamaEndpoint);
+    }
+
+}
